9a9ddb58e1b390978fd70e4ca13d9bd28d3d6486
novyanto <novanto1123@gmail.com>
2019-09-09 10:32:28 +0700

    Create prima.php

---
+++ prima.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Bilangan Prima</title>
+</head>
+<body>
+    <form id="form_prima" action="proses.php" method="POST">
+        <table>
+            <tr>
+                <td colspan="3" align="center">
+                    <h1>Bilangan Prima</h1>
+                </td>
+            </tr>
+ 
+            <tr>            
+                <td>Bilangan Akhir</td>
+                <td>:</td>
+                <td><input name="bil" type="text" placeholder="Silahkan Masukan Bilangan"></td>
+            </tr>
+            <tr>
+                <td colspan="4" align="center">
+                    <input type="submit" value="Cetak">
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
